98e3d359aec74fc922cb89f4e5adbd58781ec525
GGulsoy <146833487+GGulsoy@users.noreply.github.com>
2024-01-17 00:49:11 +0300

    20230117

--- Comp132HomeworkSolutions/src9/ST9.java
+++ Comp132HomeworkSolutions/src9/ST9.java
@@ -8,6 +8,9 @@ public class ST9 {
 		}
 		return rtrn;
 	}
+	public static void takeAndPrint() {
+		ArrayList<>
+	}
 	public static void main(String[] args) {
 		System.out.println(isPalindrome("chc"));
 	}
